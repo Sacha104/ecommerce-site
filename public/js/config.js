730ccb27502b7f2fdfa01bfc15b3183166861c22
Sacha104 <sacha.bloch.30@gmail.com>
2026-01-02 18:50:27 +0100

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+// public/js/config.js
+// Paramètres facilement modifiables par client
+
+window.APP_CONFIG = {
+  storeName: "Ma Boutique Démo",
+  logoText: "Ma Boutique",
+  primaryColor: "#1e90ff",
+  secondaryColor: "#111827",
+  accentColor: "#f97316"
+};
